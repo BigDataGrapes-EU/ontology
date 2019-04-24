38ba316a15dd4c304f3d65a3c8ec9fa97641877c
nikola <nikola.tulechki@ontotext.com>
2019-04-24 13:54:03 +0300

    BDG-44 removed from clauses from tarqls

--- data/Vocabulary/vocabulary.sh
+++ data/Vocabulary/vocabulary.sh
@@ -4,10 +4,10 @@
 #Measures
 curl "https://docs.google.com/spreadsheets/d/1gu1RLLgaRHAYFp9LxYPVyYl2ad6Dlo-ZbqakyXJGeYo/gviz/tq?tqx=out:csv&sheet=Measures" | ../bin/my-tarql "-d , --stdin" ../../model/prefixes.ttl measures.tarql > rdf/measures.ttl
 #Attributes
-curl "https://docs.google.com/spreadsheets/d/1gu1RLLgaRHAYFp9LxYPVyYl2ad6Dlo-ZbqakyXJGeYo/gviz/tq?tqx=out:csv&sheet=Attributes" | ../bin/my-tarql "-d , " ../../model/prefixes.ttl attributes.tarql > rdf/attributes.ttl
+curl "https://docs.google.com/spreadsheets/d/1gu1RLLgaRHAYFp9LxYPVyYl2ad6Dlo-ZbqakyXJGeYo/gviz/tq?tqx=out:csv&sheet=Attributes" | ../bin/my-tarql "-d , --stdin" ../../model/prefixes.ttl attributes.tarql > rdf/attributes.ttl
 
 #Dimensions
-curl "https://docs.google.com/spreadsheets/d/1gu1RLLgaRHAYFp9LxYPVyYl2ad6Dlo-ZbqakyXJGeYo/gviz/tq?tqx=out:csv&sheet=Dimensions" | ../bin/my-tarql "-d , " ../../model/prefixes.ttl dimensions.tarql > rdf/dimensions.ttl
+curl "https://docs.google.com/spreadsheets/d/1gu1RLLgaRHAYFp9LxYPVyYl2ad6Dlo-ZbqakyXJGeYo/gviz/tq?tqx=out:csv&sheet=Dimensions" | ../bin/my-tarql "-d , --stdin" ../../model/prefixes.ttl dimensions.tarql > rdf/dimensions.ttl
 
 #RDF-ize coded list master sheet
 curl "https://docs.google.com/spreadsheets/d/19fdrdisQqihUN68_lb-VEGVgxxEock-IIdDWUPfVUMA/gviz/tq?tqx=out:csv&sheet=Master" | ../bin/my-tarql "-d , --stdin" ../../model/prefixes.ttl codedLists.tarql > rdf/codedLists.ttl
