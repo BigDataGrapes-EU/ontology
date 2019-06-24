3ef88ac399705cfe13ca25138b1183e16e15a37d
Arnaud Charleroy <arnaud.charleroy@inra.fr>
2019-06-24 18:41:08 +0200

    Add Sensory models

--- data/INRA/sensory/beforeBottling/sensory.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-cat "/home/charlero/GIT/GITLAB/data-integration/csvExample/sensory_analysis.csv" | ../../../bin/my-tarql -v "-d , --stdin" ../../../../model/prefixes.ttl sensory.tarql > sensory.ttl
