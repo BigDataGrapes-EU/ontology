d03750e29a34d01e55796fd2b16ac0da7d41f871
Arnaud Charleroy <arnaud.charleroy@inra.fr>
2019-06-28 12:30:23 +0200

    Update data

---
+++ data/INRA/plot_management/harvestData/harvestData.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+curl "https://docs.google.com/spreadsheets/d/1lWEwwonC0Dg8MAWDSVm7GV184N7jGt-IhjXmdE4Y-oo/gviz/tq?tqx=out:csv&gid=2017_Harvest_Forms" | ../../../bin/my-tarql "-d , --stdin" ../../../../model/prefixes.ttl harvestData.tarql > sample.ttl
