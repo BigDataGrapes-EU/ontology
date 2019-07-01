ddfe9a9da1f87f33495ae5b2a13906a98a7ff28e
nikola <nikola.tulechki@ontotext.com>
2019-07-01 16:42:46 +0300

    Merge branch 'inra-mapping' of /home/nikola/projects/bdg/github/ontology with conflicts.

--- data/INRA/sensory_analysis/afterBottling/2017/sensory.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-curl "https://docs.google.com/spreadsheets/d/19JIqzGl6Zjqp9wdKo1au9ZzO6PEkeRIfRY1FbnQLyL0/gviz/tq?tqx=out:csv&gid=2017_Inertis_MAC" | ../../../../bin/my-tarql "-d , --stdin" ../../../../../model/prefixes.ttl sensory.tarql > sample.ttl
