b060f4ff345f09519627c947cd2be38762489d7c
Coraline Damasio <coraline.damasio@inra.fr>
2020-01-28 16:49:22 +0100

    grape maturation data transformation schema added

--- data/INRA/data/plot_management/maturation_data/run.sh
+++ data/INRA/data/plot_management/maturation_data/run.sh
@@ -1,2 +1,6 @@
+
+#dans terminal écrire bash run.sh
+
 #!/usr/bin/env bash
-curl "https://docs.google.com/spreadsheets/d/103zwveRlsPy890GfJKqwlJnUOgfgwdOH/gviz/tq?tqx=out:csv&gid=grape_maturation_silex_Ontorefine" | ../../../../bin/my-tarql "-d , --stdin" ../../../../../model/prefixes.ttl initialMustData.tarql > grapeMaturationData.ttl
+#bien regarder ce qui est avant gviz, après gid et à la fin fichier tarql
+curl "https://docs.google.com/spreadsheets/d/103zwveRlsPy890GfJKqwlJnUOgfgwdOH/gviz/tq?tqx=out:csv&gid=grape_maturation_silex_v1" | ../../../../bin/my-tarql "-d , --stdin" ../../../../../model/prefixes.ttl grapeMaturationData.tarql > grapeMaturationData.ttl
