e558e0e523ce8465d26bbeab52a7fa9cc44ec39a
Coraline Damasio <coraline.damasio@inra.fr>
2020-02-04 16:12:29 +0100

    lab analysis during fermentation transformation

---
+++ data/INRA/data/lab_fermentation_data/run.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+curl "https://docs.google.com/spreadsheets/d/1hOI1eKADaKe7lHwiV7QQ4dYMS8eObbZa/gviz/tq?tqx=out:csv&gid=84986158" | ../../../../bin/my-tarql "-d , --stdin" ../../../../../model/prefixes.ttl labFermentationData.tarql > labFermentation.ttl
