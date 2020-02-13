10119a0b915568177a49e13c0d2caa1f4a9dfb65
Arnaud Charleroy <arnaud.charleroy@inra.fr>
2020-02-13 12:27:37 +0100

    Correct tarql generation

--- data/INRA/data/lab_wine_data/run.sh
+++ data/INRA/data/lab_wine_data/run.sh
@@ -1,2 +1,2 @@
-curl "https://docs.google.com/spreadsheets/d/1yWSZ8pz5XkOHJkyHE3agha_d9TG2lk_W/gviz/tq?tqx=out:csv&gid=1339982375" | ../../../../bin/my-tarql "-d , --stdin" ../../../../../model/prefixes.ttl labWineData.tarql > labWine.ttl
+curl "https://docs.google.com/spreadsheets/d/1yWSZ8pz5XkOHJkyHE3agha_d9TG2lk_W/gviz/tq?tqx=out:csv&gid=1339982375" | ../../../bin/my-tarql "-d , --stdin"  ../../../../model/prefixes.ttl labWineData.tarql > labWine.ttl
 
