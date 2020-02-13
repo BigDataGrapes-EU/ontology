f7d81289307e6d0101acee80cdf9b2a1b18b17f5
Arnaud Charleroy <arnaud.charleroy@inra.fr>
2020-02-13 10:52:40 +0100

    Update initialMust and mustDuring fermentation data

--- data/INRA/data/lab_fermentation_data/run.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-curl "https://docs.google.com/spreadsheets/d/1hOI1eKADaKe7lHwiV7QQ4dYMS8eObbZa/gviz/tq?tqx=out:csv&gid=84986158" | ../../../../bin/my-tarql "-d , --stdin" ../../../../../model/prefixes.ttl labFermentationData.tarql > labFermentation.ttl
