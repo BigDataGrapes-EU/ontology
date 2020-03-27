ab20a2d6c4ac8f80ea4f6de97497413295aa8330
Arnaud Charleroy <arnaud.charleroy@inra.fr>
2020-03-27 11:27:28 +0100

    Update plotSoil to plot texture

---
+++ data/INRA/data/plot_management/plot_soil_texture/run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+curl "https://docs.google.com/spreadsheets/d/1KwJQKEBFc019T5qkrMjy9zyHnozs4rmVpbkX6wRvvEw/gviz/tq?tqx=out:csv&gid=2017_Harvest_Forms" | ../../../../bin/my-tarql "-d , --stdin" ../../../../../model/prefixes.ttl plotSoilTexture.tarql > rdf/plotSoilTexture.ttl
