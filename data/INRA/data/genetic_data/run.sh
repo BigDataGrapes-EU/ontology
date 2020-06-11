ee1b70e66a046ec35d275afed406785f3e07ea1e
Arnaud Charleroy <arnaud.charleroy@inra.fr>
2020-06-12 01:16:05 +0200

    Correct genetic

---
+++ data/INRA/data/genetic_data/run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+curl "https://docs.google.com/spreadsheets/d/1sgCqGrwN9ULCVvBU39rZ99BMxbYCbLFPlrRnL6GPwBg/gviz/tq?tqx=out:csv&gid=42966062" | ../../../bin/my-tarql "-d , --stdin" ../../../../model/prefixes.ttl accessions.tarql > rdf/accessions.ttl
+curl "https://docs.google.com/spreadsheets/d/1Ncg0a5JJlt0OLamJhiz5jU8ox9FMV8UCpLplRDmjptw/gviz/tq?tqx=out:csv&gid=1439917877" | ../../../bin/my-tarql "-d , --stdin" ../../../../model/prefixes.ttl varieties.tarql > rdf/varieties.ttl
