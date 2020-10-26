aacbde5ab9f8a8e5f15a16d33ac254e6295a9db3
Arnaud Charleroy <arnaud.charleroy@inra.fr>
2020-10-26 01:01:58 +0100

    Execute script

--- data/INRA/data/run_global.sh
+++ data/INRA/data/run_global.sh
@@ -7,39 +7,39 @@ DIR_PATH=$(dirname $ABSOLUTE_PATH)
 echo $ABSOLUTE_PATH
 echo $DIR_PATH 
 
-cd ${ABSOLUTE_PATH}/climatic_data/ && sh run.sh   \
-&& cd ${ABSOLUTE_PATH}/genetic_data/ && sh run.sh   \
-&& cd ${ABSOLUTE_PATH}/plot_management/maturation_data/ && sh run.sh   \
-&& cd ${ABSOLUTE_PATH}/plot_management/harvest_data/ && sh run.sh   \
-&& cd ${ABSOLUTE_PATH}/plot_management/irrigation/ && sh run.sh   \
-&& cd ${ABSOLUTE_PATH}/plot_management/plot_data/ && sh run.sh   \
-&& cd ${ABSOLUTE_PATH}/plot_management/plot_soil_texture/ && sh run.sh   \
-&& cd ${ABSOLUTE_PATH}/sensory_analysis/beforeBottling/ && sh run.sh   \
-&& cd ${ABSOLUTE_PATH}/sensory_analysis/afterBottling/ && sh run.sh   \
-&& cd ${ABSOLUTE_PATH}/winemaking_activities/pressing_objects/ && sh run.sh   \
-&& cd ${ABSOLUTE_PATH}/winemaking_activities/fermentation_objects/ && sh run.sh   \
-&& cd ${ABSOLUTE_PATH}/winemaking_activities/laboratory/must/initial_must_data/ && sh run.sh   \
-&& cd ${ABSOLUTE_PATH}/winemaking_activities/laboratory/must/laboratory_analysis_fermentation_data/ && sh run.sh   \
-&& cd ${ABSOLUTE_PATH}/winemaking_activities/laboratory/wine/lab_wine_data/ && sh run.sh   \
+cd ${DIR_PATH}/climatic_data/ && sh run.sh   \
+&& cd ${DIR_PATH}/genetic_data/ && sh run.sh   \
+&& cd ${DIR_PATH}/plot_management/maturation_data/ && sh run.sh   \
+&& cd ${DIR_PATH}/plot_management/harvest_data/ && sh run.sh   \
+&& cd ${DIR_PATH}/plot_management/irrigation/ && sh run.sh   \
+&& cd ${DIR_PATH}/plot_management/plot_data/ && sh run.sh   \
+&& cd ${DIR_PATH}/plot_management/plot_soil_texture/ && sh run.sh   \
+&& cd ${DIR_PATH}/sensory_analysis/beforeBottling/ && sh run.sh   \
+&& cd ${DIR_PATH}/sensory_analysis/afterBottling/ && sh run.sh   \
+&& cd ${DIR_PATH}/winemaking_activities/pressing_objects/ && sh run.sh   \
+&& cd ${DIR_PATH}/winemaking_activities/fermentation_objects/ && sh run.sh   \
+&& cd ${DIR_PATH}/winemaking_activities/laboratory/must/initial_must_data/ && sh run.sh   \
+&& cd ${DIR_PATH}/winemaking_activities/laboratory/must/laboratory_analysis_fermentation_data/ && sh run.sh   \
+&& cd ${DIR_PATH}/winemaking_activities/laboratory/wine/lab_wine_data/ && sh run.sh   \
 
 
-# && cd ${ABSOLUTE_PATH}/plot_management/link_harvest_area_berry/ && sh run.sh   \
+# && cd ${DIR_PATH}/plot_management/link_harvest_area_berry/ && sh run.sh   \
 cd $DIR_PATH
 
-rm -R ${ABSOLUTE_PATH}/rdf
-mkdir -p ${ABSOLUTE_PATH}/rdf
-find -iname '*.ttl' -maxdepth 1 -exec cp -f {} ${ABSOLUTE_PATH}/rdf \;
-rm ${ABSOLUTE_PATH}/rdf/sample.ttl
-sed -i 's/http:\/\/data.bigdatagrapes.eu\/resource\///g' ${ABSOLUTE_PATH}/rdf/*.ttl
+rm -R ${DIR_PATH}/rdf
+mkdir -p ${DIR_PATH}/rdf
+find -iname '*.ttl'  -exec cp -f {} ${DIR_PATH}/rdf \;
+rm ${DIR_PATH}/rdf/sample.ttl
+sed -i 's/http:\/\/data.bigdatagrapes.eu\/resource\///g' ${DIR_PATH}/rdf/*.ttl
 
-rm ${ABSOLUTE_PATH}/global.ttl && touch ${ABSOLUTE_PATH}/global.ttl
-[ -f ${ABSOLUTE_PATH}/global_datasets.ttl ] && rm ${ABSOLUTE_PATH}/global_datasets.ttl && touch ${ABSOLUTE_PATH}/global_datasets.ttl
+rm ${DIR_PATH}/global.ttl && touch ${DIR_PATH}/global.ttl
+[ -f ${DIR_PATH}/global_datasets.ttl ] && rm ${DIR_PATH}/global_datasets.ttl && touch ${DIR_PATH}/global_datasets.ttl
 
-cat ${ABSOLUTE_PATH}/rdf/*datasets.ttl >> ${ABSOLUTE_PATH}/global_datasets.ttl
-rm ${ABSOLUTE_PATH}/rdf/*datasets.ttl
+cat ${DIR_PATH}/rdf/*datasets.ttl >> ${DIR_PATH}/global_datasets.ttl
+rm ${DIR_PATH}/rdf/*datasets.ttl
 
-cat ${ABSOLUTE_PATH}/../model/prefixes.ttl >> ${ABSOLUTE_PATH}/global.ttl
-echo "" >> ${ABSOLUTE_PATH}/global.ttl
-for filename in ${ABSOLUTE_PATH}/rdf/*.ttl; do 
-    echo "$(grep -v "@prefix" $filename)" >>${ABSOLUTE_PATH}/global.ttl
+cat ${DIR_PATH}/../model/prefixes.ttl >> ${DIR_PATH}/global.ttl
+echo "" >> ${DIR_PATH}/global.ttl
+for filename in ${DIR_PATH}/rdf/*.ttl; do 
+    echo "$(grep -v "@prefix" $filename)" >>${DIR_PATH}/global.ttl
 done
