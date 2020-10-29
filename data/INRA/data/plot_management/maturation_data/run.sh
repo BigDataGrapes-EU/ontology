170117141255c968f1034a756ab04bc9abab554e
Arnaud Charleroy <arnaud.charleroy@inra.fr>
2020-10-29 12:55:28 +0100

    Update scripts and dataset definitions

--- data/INRA/data/plot_management/maturation_data/run.sh
+++ data/INRA/data/plot_management/maturation_data/run.sh
@@ -8,10 +8,10 @@
 
 curl "https://docs.google.com/spreadsheets/d/103zwveRlsPy890GfJKqwlJnUOgfgwdOH/gviz/tq?tqx=out:csv&gid=2010910138" | ../../../../bin/my-tarql "-d , --stdin" ../../../../../model/prefixes.ttl grapeMaturationBrix.tarql > rdf/grapeMaturationBrix.ttl
 
-# curl "https://docs.google.com/spreadsheets/d/103zwveRlsPy890GfJKqwlJnUOgfgwdOH/gviz/tq?tqx=out:csv&gid=2068342678" | ../../../../bin/my-tarql "-d , --stdin" ../../../../../model/prefixes.ttl grapeMaturationPh.tarql > grapeMaturationPh.ttl
+curl "https://docs.google.com/spreadsheets/d/103zwveRlsPy890GfJKqwlJnUOgfgwdOH/gviz/tq?tqx=out:csv&gid=2068342678" | ../../../../bin/my-tarql "-d , --stdin" ../../../../../model/prefixes.ttl grapeMaturationPh.tarql > rdf/grapeMaturationPh.ttl
 
-# curl "https://docs.google.com/spreadsheets/d/103zwveRlsPy890GfJKqwlJnUOgfgwdOH/gviz/tq?tqx=out:csv&gid=796506311" | ../../../../bin/my-tarql "-d , --stdin" ../../../../../model/prefixes.ttl grapeMaturationTotalacidity.tarql > grapeMaturationTotalacdity.ttl
+curl "https://docs.google.com/spreadsheets/d/103zwveRlsPy890GfJKqwlJnUOgfgwdOH/gviz/tq?tqx=out:csv&gid=796506311" | ../../../../bin/my-tarql "-d , --stdin" ../../../../../model/prefixes.ttl grapeMaturationTotalacidity.tarql > rdf/grapeMaturationTotalacdity.ttl
 
-# curl "https://docs.google.com/spreadsheets/d/103zwveRlsPy890GfJKqwlJnUOgfgwdOH/gviz/tq?tqx=out:csv&gid=163570199" | ../../../../bin/my-tarql "-d , --stdin" ../../../../../model/prefixes.ttl grapeMaturationProbablealcohol.tarql > grapeMaturationProbablealcohol.ttl
+curl "https://docs.google.com/spreadsheets/d/103zwveRlsPy890GfJKqwlJnUOgfgwdOH/gviz/tq?tqx=out:csv&gid=163570199" | ../../../../bin/my-tarql "-d , --stdin" ../../../../../model/prefixes.ttl grapeMaturationProbablealcohol.tarql > rdf/grapeMaturationProbablealcohol.ttl
 
-# curl "https://docs.google.com/spreadsheets/d/103zwveRlsPy890GfJKqwlJnUOgfgwdOH/gviz/tq?tqx=out:csv&gid=1956426450" | ../../../../bin/my-tarql "-d , --stdin" ../../../../../model/prefixes.ttl grapeMaturationSugars.tarql > grapeMaturationSugars.ttl
+curl "https://docs.google.com/spreadsheets/d/103zwveRlsPy890GfJKqwlJnUOgfgwdOH/gviz/tq?tqx=out:csv&gid=1956426450" | ../../../../bin/my-tarql "-d , --stdin" ../../../../../model/prefixes.ttl grapeMaturationSugars.tarql > rdf/grapeMaturationSugars.ttl
